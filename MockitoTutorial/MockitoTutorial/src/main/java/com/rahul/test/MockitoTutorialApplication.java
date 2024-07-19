8d061602a998c1385628ae56ad2975b41e4981bb
rahulsubh <102258593+rahulsubh@users.noreply.github.com>
2024-07-19 15:45:52 +0530

    first Successful test

---
+++ MockitoTutorial/MockitoTutorial/src/main/java/com/rahul/test/MockitoTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.rahul.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoTutorialApplication.class, args);
+	}
+
+}
---
+++ MockitoTutorial/MockitoTutorial/src/test/java/com/rahul/test/MockitoTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
